86a060c10bcba3d5907e39f853e6a87fcf738c21
Nurlan Guliyev <161559178+whoami2436@users.noreply.github.com>
2024-05-12 11:38:32 -0700

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,1 @@
-# Coders yaratmaq
-# hesab1 = Coders(123456, 1000)
-# hesab1.balans_goster()
-# hesab1.balans_artir(500)
-# hesab1.pul_cixar(200)
 
-# # Kredit hesabı yaratmaq
-# kredit_hesab1 = Kredit(654321, 2000, 10000)
-# kredit_hesab1.balans_goster()
-# kredit_hesab1.kredit_ver()
-# kredit_hesab1.kredit_odenisi()
